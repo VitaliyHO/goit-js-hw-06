10cc99468166f960fc67c0df1342384b50adccd0
VitaliyGO <shuricks007@gmail.com>
2022-08-13 17:14:01 +0300

    finished first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,18 @@
 'use strict'
 
-const categories = document.querySelectorAll('h2')
+const categories = document.querySelectorAll('h2');
+const list = document.querySelectorAll('.item>ul');
+const firstListElements = list[0].children;
+const secondListElements = list[1].children;
+const thirdListElements = list[2].children;
 
 
+console.log('Number of categories: ', categories.length);
 
-console.log('Number of categories: ', categories.length)
+console.log('Category: ', categories[0].textContent);
+console.log('Elements: ', firstListElements.length)
+console.log('Category: ', categories[1].textContent);
+console.log('Elements: ', secondListElements.length)
+console.log('Category: ', categories[2].textContent);
+console.log('Elements: ', thirdListElements.length)
+
