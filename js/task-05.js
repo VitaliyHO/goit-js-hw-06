ca81049126aeb8fe459098b54342cba33fb6144e
VitaliyGO <shuricks007@gmail.com>
2022-08-17 22:59:57 +0300

    completed fifth task

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', () =>
+    nameOutput.textContent = event.currentTarget.value
+);
