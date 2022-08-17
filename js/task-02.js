e70f9b0da6bf711dab61917d04649a1c6772d9e5
VitaliyGO <shuricks007@gmail.com>
2022-08-17 23:05:04 +0300

    deleted redundant commented code

--- js/task-02.js
+++ js/task-02.js
@@ -18,31 +18,3 @@ const elements = ingredients.map(element => {
 )
 
 list.append(...elements);
-
-
-
-// const firstEl = document.createElement('li');
-// firstEl.textContent = ingredients[0];
-// firstEl.classList.add('item');
-
-// const secondEl = document.createElement('li');
-// secondEl.textContent = ingredients[1];
-// secondEl.classList.add('item');
-
-// const thirdEl = document.createElement('li');
-// thirdEl.textContent = ingredients[2];
-// thirdEl.classList.add('item');
-
-// const fourthEl = document.createElement('li');
-// fourthEl.textContent = ingredients[3];
-// fourthEl.classList.add('item');
-
-// const fifthEl = document.createElement('li');
-// fifthEl.textContent = ingredients[4];
-// fifthEl.classList.add('item');
-
-// const sixthEl = document.createElement('li');
-// sixthEl.textContent = ingredients[5];
-// sixthEl.classList.add('item');
-
-// list.append(firstEl, secondEl, thirdEl, fourthEl, fifthEl, sixthEl);
