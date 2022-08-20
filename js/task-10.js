1f240bf58691263a0233cac177bc9959f0e29c04
unknown <shuricks007@gmail.com>
2022-08-20 23:53:32 +0300

    comleted tenth task =)

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+'use strict'
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumber = document.querySelector('#controls>input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const container = document.querySelector('#boxes');
+
+inputNumber.addEventListener('click', event => event.currentTarget.value = '')
+
+createButton.addEventListener('click', createBoxes);
+
+function createBoxes(amount){
+  amount = inputNumber.value;
+  let size = 20;
+  let boxArr = [];
+
+  for(let i = 1; i <= amount; i += 1){
+    const box = document.createElement('div');
+    box.classList.add('box')
+    box.style.width = `${size + i * 10}px`;
+    box.style.height = `${size + i * 10}px` ;
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+
+    boxArr.push(box);
+  }
+
+  container.append(...boxArr);
+};
+
+destroyButton.addEventListener('click', destroyBoxes => container.textContent = '');
+
