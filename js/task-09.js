04c973b583cf14c42592c1a2a96fedc21e6c6e87
unknown <shuricks007@gmail.com>
2022-08-20 15:58:45 +0300

    comleted ninth task

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('body');
+const colorNumber = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', changeBgColor);
+
+function changeBgColor() {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  colorNumber.textContent = `${getRandomHexColor()}`;
+}
