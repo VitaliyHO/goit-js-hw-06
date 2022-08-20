27d347c934cca120e62637e66509d5e5594afa64
unknown <shuricks007@gmail.com>
2022-08-20 10:55:00 +0300

    completed eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+'use strict'
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event){
+    event.preventDefault();
+
+    const {
+        elements: {email, password}
+    } = event.currentTarget;
+
+    if(email.value === '' || password.value === ''){
+        return console.error('All area must be fill');
+    }else{
+        console.log(`Email: ${email.value}, Password: ${password.value}`);
+    }
+
+    event.currentTarget.reset();
+}
