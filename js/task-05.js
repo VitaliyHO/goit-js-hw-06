5b891bea458763f714ac5a91ec8687c11b58dfe8
VitaliyGO <shuricks007@gmail.com>
2022-08-21 18:42:05 +0300

    update fifth task

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,11 @@
 const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
-nameInput.addEventListener('input', () =>
-    nameOutput.textContent = event.currentTarget.value
-);
+
+nameInput.addEventListener('input', event =>{
+    if(event.currentTarget.value === ''){
+        return nameOutput.textContent = 'Anonymous';
+    }
+        return nameOutput.textContent = event.currentTarget.value;
+    });
+
