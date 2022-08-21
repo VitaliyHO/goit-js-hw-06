cb0551201153d227409ce7e9a41dc484c9576d0d
VitaliyGO <shuricks007@gmail.com>
2022-08-21 18:16:06 +0300

    added method forEach to first task

--- js/task-01.js
+++ js/task-01.js
@@ -1,18 +1,11 @@
 'use strict'
 
 const categories = document.querySelectorAll('h2');
-const list = document.querySelectorAll('.item>ul');
-const firstListElements = list[0].children;
-const secondListElements = list[1].children;
-const thirdListElements = list[2].children;
-
+const list = document.querySelectorAll('.item');
 
 console.log('Number of categories: ', categories.length);
 
-console.log('Category: ', categories[0].textContent);
-console.log('Elements: ', firstListElements.length)
-console.log('Category: ', categories[1].textContent);
-console.log('Elements: ', secondListElements.length)
-console.log('Category: ', categories[2].textContent);
-console.log('Elements: ', thirdListElements.length)
-
+list.forEach(element => {
+    return  console.log('Category: ', element.firstElementChild.textContent), 
+            console.log('Elements: ', element.lastElementChild.children.length);
+});
